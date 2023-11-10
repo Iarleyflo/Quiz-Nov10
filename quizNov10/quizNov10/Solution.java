d391e012d19fe576e331afe4ce5af428caf886ee
Iarleyflo <143017895+Iarleyflo@users.noreply.github.com>
2023-11-10 11:27:34 -0600

    Update Solution.java

--- quizNov10/quizNov10/Solution.java
+++ quizNov10/quizNov10/Solution.java
@@ -2,35 +2,47 @@ package quizNov10;
 
 import java.util.Scanner;
 
-public class Solution{
+public class Solution {
 
-	public static void main(String[] args) {
-		String str;
-		int[] digits;
-		Scanner keyboard = new Scanner(System.in);
-		System.out.print("String: ");
-		str = keyboard.nextLine();
-		
-		digits=getDigits(str);
-		showDigits(digits);
-	}
-	
-	public static void showDigits(int[] digits) {
-		int index;
-		
-		System.out.print("[");
-		for(index=0;index<digits.length;index++) {
-			if(index!=0) System.out.print(",");
-			System.out.print(digits[index]);
-		}
-		System.out.println("]");
-	}
+    public static void main(String[] args) {
+        String str;
+        int[] digits;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("String: ");
+        str = keyboard.nextLine();
 
-	public static int[] getDigits(String str) {
-		//*** Provide the Code to Implement this function ***
-		//*** Run Program to Test ***
-		
-		return null;
-	}
+        digits = getDigits(str);
+        showDigits(digits);
+    }
 
+    public static void showDigits(int[] digits) {
+        int index;
+
+        System.out.print("[");
+        for (index = 0; index < digits.length; index++) {
+            if (index != 0) System.out.print(",");
+            System.out.print(digits[index]);
+        }
+        System.out.println("]");
+    }
+
+    public static int[] getDigits(String str) {
+    	//*** Provide the Code to Implement this function ***
+    			//*** Run Program to Test ***
+        int[] result = new int[str.length()];
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (Character.isDigit(ch)) {
+                result[count++] = Character.getNumericValue(ch);
+            }
+        }
+
+        int[] finalResult = new int[count];
+        System.arraycopy(result, 0, finalResult, 0, count);
+
+        return finalResult;
+    }
 }
